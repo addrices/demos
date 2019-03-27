3363e18011297c41d23ce979374321bcba536d18
Chun <caochun@gmail.com>
2019-03-27 23:19:58 +0800

    add beans demo

---
+++ springbeans/src/main/java/cn/edu/nju/sa2017/di/RamdonPrinter.java
@@ -0,0 +1,11 @@
+package cn.edu.nju.sa2017.di;
+
+/**
+ * Created by chun on 2017/3/20.
+ */
+public class RamdonPrinter implements Printer {
+
+    public void printMessage(String message) {
+        System.out.println("abc");
+    }
+}
