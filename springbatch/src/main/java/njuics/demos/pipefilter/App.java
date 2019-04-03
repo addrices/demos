614b7e0b612fe51550fd6c1e0d4bab5743f0dac4
Chun <caochun@gmail.com>
2019-04-03 16:01:06 +0800

    update

--- springbatch/src/main/java/njuics/demos/pipefilter/App.java
+++ springbatch/src/main/java/njuics/demos/pipefilter/App.java
@@ -4,22 +4,19 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
     public static void main(String[] args) {
 
-        String[] springConfig =
-                {
-                        "spring/batch/jobs/job-hello-world.xml"
-                };
+        String[] springConfig = { "spring/batch/jobs/job-hello-world.xml" };
 
-        ApplicationContext context =
-                new ClassPathXmlApplicationContext(springConfig);
-
-        JobLauncher jobLauncher = (JobLauncher) context.getBean("jobLauncher");
-        Job job = (Job) context.getBean("helloWorldJob");
+        JobLauncher jobLauncher;
+        Job job;
+        try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(springConfig)) {
+            jobLauncher = (JobLauncher) context.getBean("jobLauncher");
+            job = (Job) context.getBean("helloWorldJob");
+        }
 
         try {
 
@@ -28,6 +25,7 @@ public class App {
 
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
         }
 
         System.out.println("Done");
